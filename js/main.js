1a495c637a834da74a92e15f592fb88e0169ed43
powerteks <jit.prophet@gmail.com>
2018-10-21 22:51:46 +0300

    add css and js for index.html mobile version

---
+++ js/main.js
@@ -0,0 +1,37 @@
+var navMain    = document.querySelector('.main-nav'),
+    navToggle  = document.querySelector('.main-nav__button-toggle'),
+    commSlide  = document.querySelectorAll('.comments__item'),
+    commPrev   = document.querySelector('.comments__button--previous'),
+    commNext   = document.querySelector('.comments__button--next'),
+    slideIndex = 0;
+    showSlide(slideIndex);
+
+navMain.classList.remove('main-nav--no-js');
+
+navToggle.addEventListener('click', function() {
+  if (navMain.classList.contains('main-nav--closed')) {
+      navMain.classList.remove('main-nav--closed');
+      navMain.classList.add('main-nav--opened');
+  } else {
+      navMain.classList.remove('main-nav--opened');
+      navMain.classList.add('main-nav--closed');
+  }
+});
+
+commPrev.addEventListener('click', function(n) {
+     showSlide(slideIndex -= 1);
+});
+
+commNext.addEventListener('click', function(n) {
+     showSlide(slideIndex += 1);
+});
+
+function showSlide(n) {
+  var i
+  if ( n > commSlide.length-1) { slideIndex = 0 };
+  if ( n < 0 ) { slideIndex = commSlide.length-1 };
+  for ( i = 0; i < commSlide.length; i++) {
+    commSlide[i].classList.remove('comments__item-show');
+  };
+  commSlide[slideIndex].classList.add('comments__item-show');
+};
